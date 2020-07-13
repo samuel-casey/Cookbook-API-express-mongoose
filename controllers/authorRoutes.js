c90197e7cadd1b6285450ccae83161e2692a4346
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2020-07-13 11:45:05 -0700

    Changes made to routes

--- controllers/authorRoutes.js
+++ controllers/authorRoutes.js
@@ -1,22 +1,47 @@
-const express = require('express')
-const router = express.Router()
-
-
-// Require the Author Controller.
-const authorController = require('./authors');
+const express = require("express");
+const router = express.Router();
+const Author = require('../models/Author');
+const Cookbook = require("../models/Cookbook");
 // Write the route to list all authors
-router.get('/', authorController.getAuthors)
+router.get("/", (req, res) => {
+  Author.find({}).then((authors) => {
+    res.json(authors);
+  });
+});
 // Write the route to get authors by firstname
-router.get('/:name', authorController.getAuthorsByName)
+router.get("/:name", (req, res) => {
+  Author.find({ firstName: req.params.name }).then((authors) => {
+    res.json(authors);
+  });
+});
 // Write the route to create an author:
-router.post('/', authorController.createAuthor)
+router.post("/", (req, res) => {
+  Author.create(req.body).then((author) => {
+    res.json(author);
+  });
+});
 // Write the route to update an author
-router.put('/:name', authorController.updateAuthor)
+router.put("/:name", (req, res) => {
+  Author.findOneAndUpdate({ firstName: req.params.name }, req.body).then(
+    (author) => {
+      res.json(author);
+    }
+  );
+});
 
 //Write a route to delete an author
-router.delete('/:id', authorController.deleteAuthor)
-// Write the route to get all 
-router.get('/:id/cookbooks', authorController.getAuthorsWithCookbooks)
+router.delete("/:id", (req, res) => {
+  Author.delete({ _id: req.params.id }).then((author) => {
+    res.json(author);
+  });
+});
+// Write the route to get all
+router.get("/:id/cookbooks", (req, res) => {
+  Author.find({ _id: req.params.id })
+    .populate("cookbooks")
+    .then((authors) => {
+      res.json(authors);
+    });
+});
 
-
-module.exports = router
+module.exports = router;
--- controllers/cookbookRoutes.js
+++ controllers/cookbookRoutes.js
@@ -1,20 +1,43 @@
-const express = require('express')
-const router = express.Router()
-
-// Require the Cookbook controller.
-const cookbookController = require('../controllers/cookbooks')
+const express = require("express");
+const router = express.Router();
+const Cookbook = require('../models/Cookbook');
 
 // Write the route to list all cookbooks
-router.get('/', cookbookController.getAllCookbooks)
+router.get("/", (req, res) => {
+  Cookbook.find({}).then((cookbooks) => {
+    res.json(cookbooks);
+  });
+});
 // Write the route to get cookbook by title
-router.get('/title/:title', cookbookController.getCookbookByTitle)
+router.get("/title/:title", (req, res) => {
+    
+  Cookbook.find({ title: req.params.title }).then((cookbook) => {
+    res.json(cookbook);
+  });
+});
 // Write the route to get cookbook by year published
-router.get('/year/:year', cookbookController.getCookbooksByYear)
+router.get("/year/:year", (req, res) => {
+  Cookbook.find({ yearPublished: req.params.year }).then((cookbooks) => {
+    res.json(cookbooks);
+  });
+});
 // Write the route to create a cookbook
-router.post('/', cookbookController.createCookbook)
+router.post("/", (req, res) => {
+  Cookbook.create(req.body).then((cookbook) => {
+    res.json(cookbook);
+  });
+});
 // Write the route to update a cookbook
-router.put('/:id', cookbookController.updateCookbook)
+router.put("/:id", (req, res) => {
+  Cookbook.findByIdAndUpdate(req.params.id, req.body).then((cookbook) => {
+    res.json(cookbook);
+  });
+});
 // Write the route to delete the cookbook by title
-router.delete('/:id', cookbookController.deleteCookbook)
+router.delete("/:title", (req, res) => {
+  Cookbook.deleteOne({ title: req.params.title }).then((cookbook) => {
+    res.json(cookbook);
+  });
+});
 
 module.exports = router;
